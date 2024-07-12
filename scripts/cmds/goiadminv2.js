7806ce61845881ff65eeb2716f8a71ce53282814
Absky24 <galvezv781@gmail.com>
2024-07-12 13:32:00 +0800

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -1,30 +1,24 @@
 module.exports = {
-	config: {
-		name: "goiadminv2",
-		version: "1.0",
-		author: "Cliff",
-		countDown: 5,
-		role: 0,
-		shortDescription: "sarcasm",
-		longDescription: "sarcasm",
-		category: "reply",
-	},
-	onStart: async function () {},
-	onChat: async function ({ event, message, getLang, api }) {
-		const msg = [
-			"Stop mentioning my creator, he's busy 😗",
-			"My Creator is currently offline 😢",
-			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
-			"busy pa ata yun kaya mag-antay ka",
-			"Sorry, naka bebetime pa don't disturb him 🙄",
-			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
-			"Another tag in my Creator, i will kick your fucking ass"
-		];
+  config: {
+    name: "goiadminv2",
+    version: "1.0",
+    author: "Cliff",
+    countDown: 5,
+    role: 0,
+    shortDescription: "sarcasm",
+    longDescription: "sarcasm",
+    category: "reply",
+  },
+  onStart: async function () {},
+  onChat: async function ({ event, message, getLang, api }) {
+    const msg = [
+      "why are you mentioned my master?, do you have any problem?🙄","do you like my master, thats why you mention him?😏","one more mention to my master, I will remove you from the group😡","sorry my master is currently busy, please try to mention him later","bakit mo siya binabanggit? ako ang girlfriend niya!","My master said that when he is mentioned, I should ask why, so why did you mention my master?","don't take my master away from me😭"
+    ];
 
-		const CliffRegex = /^(@Cliff Vincent V. Torrevillas 󱢏)$/i;
-		if (event.body && CliffRegex.test(event.body)) {
-			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
-			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
-		}
-	},
+    const CliffRegex = /^(@Vincent Galvez)$/i;
+    if (event.body && CliffRegex.test(event.body)) {
+      api.setMessageReaction("😡", event.messageID, (err) => {}, true);
+      return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
+    }
+  },
 };
